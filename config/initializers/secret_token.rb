8c750fa0e687653db03a1c6d0f8dfdd44b513b73
rnwaobasi <rnwaobasi@gmail.com>
2012-09-06 00:37:29 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FauxCinema::Application.config.secret_token = '99a29760e39f7f6c329b2ba0f24cecbfbec103ac73be03e5c66db53aac25480ba4eb1c64f16c13161146add84d1c84f14617e5e3bdbe0c975174bdcd070d9836'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+FauxCinema::Application.config.session_store :cookie_store, key: '_faux_cinema_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# FauxCinema::Application.config.session_store :active_record_store
