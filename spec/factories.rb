ee5b383836b45f3a47d138d36942c03e0572313a
rnwaobasi <rnwaobasi@gmail.com>
2012-09-23 19:29:53 -0400

    Finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Richard Nwaobasi"
+    email    "rnwaobasi@example.com"
+    password "foobar"
+    password_confirmation "foobar"
+  end
+end
