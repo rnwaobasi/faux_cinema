a3eced58c86e5f92c3ce5404cb40cfeb8f8a97a1
rnwaobasi <rnwaobasi@gmail.com>
2012-10-18 03:21:10 -0400

    Finish sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
+      sign_in @user
       flash[:success] = "Welcome to Project Faux Cinema!"
       redirect_to @user
     else
